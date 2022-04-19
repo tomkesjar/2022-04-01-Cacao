93983967150022c4cc9a6146aadc77a77ee66b1e
tkesjar <tom.kesjar.elte@gmail.com>
2022-04-19 13:40:46 +0200

    add panel refresh, fix jungle tile placement, add worker tile placement evaluation

--- src/main/java/tiles/WorkerTile.java
+++ src/main/java/tiles/WorkerTile.java
@@ -1,10 +1,15 @@
 package tiles;
 
 import game.Game;
+import javafx.util.Pair;
 import players.Player;
 import players.PlayerColour;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class WorkerTile extends AbstractTile {
 
@@ -34,60 +39,10 @@ public class WorkerTile extends AbstractTile {
         this.colour = colour;
     }
 
-    /*
-        public String getRightNeighbour() {
-            return rightNeighbour;
-        }
-
-        public void setRightNeighbour(String rightNeighbour) {
-            this.rightNeighbour = rightNeighbour;
-        }
-
-        public String getUpNeighbour() {
-            return upNeighbour;
-        }
-
-        public void setUpNeighbour(String upNeighbour) {
-            this.upNeighbour = upNeighbour;
-        }
-
-        public String getLeftNeighbour() {
-            return leftNeighbour;
-        }
-
-        public void setLeftNeighbour(String leftNeighbour) {
-            this.leftNeighbour = leftNeighbour;
-        }
-
-        public String getDownNeighbour() {
-            return downNeighbour;
-        }
-
-        public void setDownNeighbour(String downNeighbour) {
-            this.downNeighbour = downNeighbour;
-        }
-    */
     public PlayerColour getColour() {
         return colour;
     }
 
-    /*
-    public boolean isWorkerNumbersMatchOnEachSide(WorkerTile otherTile){
-        if (this.getColour() != otherTile.getColour()) {
-            return false;
-        }
-        boolean original = this.getRightNeighbour() == otherTile.getRightNeighbour() && this.getUpNeighbour() == otherTile.getUpNeighbour() && this.getLeftNeighbour() == otherTile.getLeftNeighbour() && this.getDownNeighbour() == otherTile.getDownNeighbour();
-        otherTile.turnRightWorkersNinetyDegrees();
-        boolean rotateOne = this.getRightNeighbour() == otherTile.getRightNeighbour() && this.getUpNeighbour() == otherTile.getUpNeighbour() && this.getLeftNeighbour() == otherTile.getLeftNeighbour() && this.getDownNeighbour() == otherTile.getDownNeighbour();
-        otherTile.turnRightWorkersNinetyDegrees();
-        boolean rotateTwo = this.getRightNeighbour() == otherTile.getRightNeighbour() && this.getUpNeighbour() == otherTile.getUpNeighbour() && this.getLeftNeighbour() == otherTile.getLeftNeighbour() && this.getDownNeighbour() == otherTile.getDownNeighbour();
-        otherTile.turnRightWorkersNinetyDegrees();
-        boolean rotateThree = this.getRightNeighbour() == otherTile.getRightNeighbour() && this.getUpNeighbour() == otherTile.getUpNeighbour() && this.getLeftNeighbour() == otherTile.getLeftNeighbour() && this.getDownNeighbour() == otherTile.getDownNeighbour();
-
-        return original || rotateOne || rotateTwo || rotateThree;
-    }
-
-     */
 
     @Override
     public String toString() {
@@ -110,10 +65,32 @@ public class WorkerTile extends AbstractTile {
     }
 
 
+    public void processNeighbours(Point coord, Game game) {
+        LinkedList<Pair<Point, Integer>> processOrder = new LinkedList<>();
+
+        List<Pair<Point, Integer>> sidesAndWorkers = Arrays.asList(
+                new Pair(new Point(coord.x - 1, coord.y), leftWorker),
+                new Pair(new Point(coord.x + 1, coord.y), rightWorker),
+                new Pair(new Point(coord.x, coord.y - 1), upWorker),
+                new Pair(new Point(coord.x, coord.y + 1), downWorker));
+
+        for (Pair<Point, Integer> side: sidesAndWorkers) {
+            AbstractTile tile = game.getBoard().getField(side.getKey().x, side.getKey().y);
+            if ((tile.getTileType().equals(TileEnum.MARKET_LOW) || tile.getTileType().equals(TileEnum.MARKET_MID) || tile.getTileType().equals(TileEnum.MARKET_HIGH))) {
+                processOrder.addLast(new Pair<>(new Point(side.getKey().x, side.getKey().y), side.getValue()));
+            } else {
+                processOrder.addFirst(new Pair<>(new Point(side.getKey().x, side.getKey().y), side.getValue()));
+            }
+
+        }
+
+        processOrder.forEach(neighbour -> processNeighbour(neighbour.getKey(), game, neighbour.getValue()));
+    }
+
     public void processRightNeighbourOfWorker(Point coord, Game game) {
         Player activePlayer = game.getPlayerList().get(game.getActivePlayer());
 
-        processNeighbour(new Point(coord.x + 1, coord.y), game, leftWorker);
+        processNeighbour(new Point(coord.x + 1, coord.y), game, rightWorker);
 
     }
 
@@ -127,18 +104,18 @@ public class WorkerTile extends AbstractTile {
     public void processDownNeighbourOfWorker(Point coord, Game game) {
         Player activePlayer = game.getPlayerList().get(game.getActivePlayer());
 
-        processNeighbour(new Point(coord.x, coord.y+1), game, leftWorker);
+        processNeighbour(new Point(coord.x, coord.y + 1), game, downWorker);
 
     }
 
     public void processUpNeighbourOfWorker(Point coord, Game game) {
         Player activePlayer = game.getPlayerList().get(game.getActivePlayer());
 
-        processNeighbour(new Point(coord.x, coord.y-1), game, leftWorker);
+        processNeighbour(new Point(coord.x, coord.y - 1), game, upWorker);
 
     }
 
-    private void processNeighbour(Point coord, Game game, int numberOfLeftWorker) {
+    private void processNeighbour(Point coord, Game game, int numberOfWorker) {
         if (!(coord.x < 0 || coord.x >= game.getBoard().getWidth() || coord.y < 0 || coord.y >= game.getBoard().getHeight())) {
             Player activePlayer = game.getPlayerList().get(game.getActivePlayer());
 
@@ -146,46 +123,46 @@ public class WorkerTile extends AbstractTile {
 
             switch (neighbourJungleTile.getTileType()) {
                 case WATER:
-                    for(int i=0; i< numberOfLeftWorker; ++i){
-                        if(activePlayer.getWaterPointIndex() < Game.getWaterPositionValueList().size()) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
+                        if (activePlayer.getWaterPointIndex() < Game.getWaterPositionValueList().size()) {
                             activePlayer.setWaterPointIndex(activePlayer.getWaterPointIndex() + 1);       //TODO: meet actual game rule
                             activePlayer.setWaterPoint(Game.getWaterPositionValue(activePlayer.getWaterPointIndex()));
                         }
                     }
                     break;
                 case TEMPLE:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setTemplePoint(activePlayer.getTemplePoint() + 1);
                     }
                     break;
                 case WORSHIP_SITE:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setWorshipSymbol(Math.min(activePlayer.getWorshipSymbol() + 1, Game.getMaxNumberOfWorshipSites()));
                     }
                     break;
                 case MINE_1:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setCoins(activePlayer.getCoins() + 1);
                     }
                     break;
                 case MINE_2:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setCoins(activePlayer.getCoins() + 2);
                     }
                     break;
                 case PLANTATION_1:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setNumberOfCacaoBean(Math.min(activePlayer.getNumberOfCacaoBean() + 1, Game.getMaxNumberOfCacaoBeans()));
                     }
                     break;
                 case PLANTATION_2:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         activePlayer.setNumberOfCacaoBean(Math.min(activePlayer.getNumberOfCacaoBean() + 2, Game.getMaxNumberOfCacaoBeans()));
                     }
                     break;
 
                 case MARKET_LOW:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         if (activePlayer.getNumberOfCacaoBean() > 0) {
                             activePlayer.setNumberOfCacaoBean(activePlayer.getNumberOfCacaoBean() - 1);
                             activePlayer.setCoins(activePlayer.getCoins() + Market.MarketPrice.LOW.getValue());
@@ -193,7 +170,7 @@ public class WorkerTile extends AbstractTile {
                     }
                     break;
                 case MARKET_MID:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         if (activePlayer.getNumberOfCacaoBean() > 0) {
                             activePlayer.setNumberOfCacaoBean(activePlayer.getNumberOfCacaoBean() - 1);
                             activePlayer.setCoins(activePlayer.getCoins() + Market.MarketPrice.MID.getValue());
@@ -201,7 +178,7 @@ public class WorkerTile extends AbstractTile {
                     }
                     break;
                 case MARKET_HIGH:
-                    for(int i=0; i< numberOfLeftWorker; ++i) {
+                    for (int i = 0; i < numberOfWorker; ++i) {
                         if (activePlayer.getNumberOfCacaoBean() > 0) {
                             activePlayer.setNumberOfCacaoBean(activePlayer.getNumberOfCacaoBean() - 1);
                             activePlayer.setCoins(activePlayer.getCoins() + Market.MarketPrice.HIGH.getValue());
