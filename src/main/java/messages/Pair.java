e497f25a9f65114947960265c7807c444564af25
tkesjar <tom.kesjar.elte@gmail.com>
2022-05-02 17:24:04 +0200

    add custom pair implementation

---
+++ src/main/java/messages/Pair.java
@@ -0,0 +1,29 @@
+package messages;
+
+import java.io.Serializable;
+
+public class Pair<K extends Object, V extends Object> extends Object implements Serializable {
+    private K key;
+    private V value;
+
+    public Pair(K k, V v) {
+        this.key = k;
+        this.value = v;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
